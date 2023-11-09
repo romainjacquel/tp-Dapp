7d9a1d22a6f0ab15bb7f10a7b1c2f4fdc29349cc
Romain JACQUEL <romainjacquel3@gmail.com>
2023-11-09 16:35:05 +0100

    Fix localhost url

--- backend/hardhat.config.ts
+++ backend/hardhat.config.ts
@@ -5,9 +5,9 @@ import "dotenv/config";
 import "hardhat-gas-reporter";
 import "solidity-coverage";
 
-const SEPOLIA_RPC_URL = process.env.SEPOLIA_RPC_URL || "";
-const PRIVATE_KEY = process.env.PRIVATE_KEY || "";
-const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY || "";
+const SEPOLIA_RPC_URL = String(process.env.SEPOLIA_RPC_URL);
+const PRIVATE_KEY = String(process.env.PRIVATE_KEY);
+const ETHERSCAN_API_KEY = String(process.env.ETHERSCAN_API_KEY);
 
 module.exports = {
 	defaultNetwork: "hardhat",
@@ -19,7 +19,7 @@ module.exports = {
 			blockConfirmations: 6,
 		},
 		localhost: {
-			url: "<http://127.0.0.1:8545>",
+			url: "http://127.0.0.1:8545",
 			chainId: 31337,
 		},
 	},
