df2df140605eb028a5ccb99ab9969a46fcc681f7
Romain JACQUEL <romainjacquel3@gmail.com>
2023-11-17 17:04:25 +0100

    Resolve conflicts

--- frontend/app/components/votesTallied.tsx
+++ frontend/app/components/votesTallied.tsx
@@ -3,16 +3,16 @@ import { FaTrophy } from "react-icons/fa";
 import useWinningProposalId from "../hooks/use-winning-proposal-id";
 
 const VotesTallied = () => {
-  const winningProposalID = useWinningProposalId();
+	const winningProposalID = useWinningProposalId();
 
-  return (
-    <Box p="4" boxShadow="md" borderRadius="md" bg="white">
-      <Heading as="h2" size="lg" m="4" display="flex" alignItems="center">
-        <FaTrophy size={32} style={{ marginRight: "10px" }} />
-        The Winning Proposal ID is: {winningProposalID}
-      </Heading>
-    </Box>
-  );
+	return (
+		<Box p="4" boxShadow="md" borderRadius="md" bg="white">
+			<Heading as="h2" size="lg" m="4" display="flex" alignItems="center">
+				<FaTrophy size={32} style={{ marginRight: "10px" }} />
+				The Winning Proposal ID is: {winningProposalID}
+			</Heading>
+		</Box>
+	);
 };
 
 export default VotesTallied;
